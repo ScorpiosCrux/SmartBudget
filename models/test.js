d51686bace564b9f81321b90d13a0651af22839e
Tyler Chen <chentyler52@gmail.com>
2022-05-25 00:19:53 -0600

    init commit and basic setup

---
+++ models/test.js
@@ -0,0 +1,11 @@
+const mongoose = requre('mongoose');
+const Schema = mongoose.Schema;
+
+const TransactionSchema = new Schema({
+    description: String,
+    cost: String,
+    date: String,
+    category: String
+});
+
+module.exports = mongoose.model('Transactions', TransactionSchema);
