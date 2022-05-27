64d30c1791ca8ea7d94589052d95cb417d6f907b
Tyler Chen <chentyler52@gmail.com>
2022-05-26 19:37:30 -0600

    added the ability to parse the request body. Also can add new transactions manually. Added some navigation buttons

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+/* This parses the request body so that we can use the variables
+This is used in app.post('transactions'... */
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.render('home')
 });
@@ -25,11 +29,21 @@ app.get('/transactions', async (req, res) => {
     res.render('transactions/index', {transactions} )               // We pass transactions to the EJS template
 });
 
+app.get('/transactions/new', async (req, res) => {
+    res.render('transactions/new');
+})
+
+app.post('/transactions', async(req, res) => {
+    const transaction = new Transaction(req.body.transaction);
+    await transaction.save();                                       // since this is an asyc function, "await" for the promise to resolve
+    res.redirect(`/transactions/${transaction._id}`);               // redirect to the transaction we just created
+})
 
 app.get('/transactions/:id', async (req, res) => {                  // :id is the id of the transaction
     const transaction = await Transaction.findById(req.params.id)   // Using the id, we find the transaction in the db
     res.render('transactions/show', {transaction})
 });
+
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
