1e7cb7f47fc04537a081277be833d57442218cd3
Tyler Chen <chentyler52@gmail.com>
2022-07-26 13:57:39 -0600

    more custom error handling with ExpressError class

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express');     //express allows us to start up a server and define routes
-const path = require('path');           //allows us to run code from any path
-const mongoose = require('mongoose');   //the interface for interacting with mongoDB
-const ejsMate = require('ejs-mate')      //allows us to further extend templating
-const methodOverride = require('method-override');  //allows other requests other than GET and POST
-const Transaction = require('./models/transaction');//I believe this allows us to use the schema and connect to the db? 
+const express = require('express'); //express allows us to start up a server and define routes
+const path = require('path'); //allows us to run code from any path
+const mongoose = require('mongoose'); //the interface for interacting with mongoDB
+const ejsMate = require('ejs-mate') //allows us to further extend templating
+const methodOverride = require('method-override'); //allows other requests other than GET and POST
+const Transaction = require('./models/transaction'); //I believe this allows us to use the schema and connect to the db? 
 const CatchAsync = require("./utils/CatchAsync")
+const ExpressError = require("./utils/ExpressError")
 
 mongoose.connect('mongodb://localhost:27017/smart-budget');
 
@@ -22,11 +23,13 @@ app.set('views', path.join(__dirname, 'views'))
 
 /* This parses the request body so that we can use the variables
 This is used in app.post('transactions'... */
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 
 //Since HTML only has get and post, we can still use "put" etc with this
 // '_method' will be the query string that is encoded in the URL
-app.use(methodOverride('_method'))          
+app.use(methodOverride('_method'))
 
 /* NOTE:
     When adding a route, order matters a lot here. Each request will take
@@ -38,45 +41,71 @@ app.get('/', (req, res) => {
 });
 
 app.get('/transactions', async (req, res) => {
-    const transactions = await Transaction.find({});                // Using Transactions we look for everything
-    res.render('transactions/index', {transactions} )               // We pass transactions to the EJS template
+    const transactions = await Transaction.find({}); // Using Transactions we look for everything
+    res.render('transactions/index', {
+        transactions
+    }) // We pass transactions to the EJS template
 });
 
 app.get('/transactions/new', async (req, res) => {
     res.render('transactions/new');
 })
 
-app.post('/transactions', async(req, res) => {
+app.post('/transactions', CatchAsync(async (req, res) => {
+    if (!req.body.transaction) throw new ExpressError('Invalid Transaction Data', 400);     // If req.body.transaction is undefined it will be false
     const transaction = new Transaction(req.body.transaction);
-    await transaction.save();                                       // since this is an asyc function, "await" for the promise to resolve
-    res.redirect(`/transactions/${transaction._id}`);               // redirect to the transaction we just created
-})
-
-app.get('/transactions/:id', CatchAsync(async (req, res, next) => {                  // :id is the id of the transaction
-        const transaction = await Transaction.findById(req.params.id)   // Using the id, we find the transaction in the db
-        res.render('transactions/show', {transaction})    
+    await transaction.save(); // since this is an asyc function, "await" for the promise to resolve
+    res.redirect(`/transactions/${transaction._id}`); // redirect to the transaction we just created
 }));
 
-app.get('/transactions/:id/edit', async (req, res) => {                  // :id is the id of the transaction
-    const transaction = await Transaction.findById(req.params.id)   // Using the id, we find the transaction in the db
-    res.render('transactions/edit', {transaction})
+app.get('/transactions/:id', CatchAsync(async (req, res, next) => { // :id is the id of the transaction
+    const transaction = await Transaction.findById(req.params.id) // Using the id, we find the transaction in the db
+    res.render('transactions/show', {
+        transaction
+    })
+}));
+
+app.get('/transactions/:id/edit', async (req, res) => { // :id is the id of the transaction
+    const transaction = await Transaction.findById(req.params.id) // Using the id, we find the transaction in the db
+    res.render('transactions/edit', {
+        transaction
+    })
 });
 
 app.put('/transactions/:id', async (req, res) => {
-    const { id } = req.params;
-    const transaction = await Transaction.findByIdAndUpdate(id, {...req.body.transaction})    // We spread the array. Basically making the array/dict args
+    const {
+        id
+    } = req.params;
+    const transaction = await Transaction.findByIdAndUpdate(id, {
+        ...req.body.transaction
+    }) // We spread the array. Basically making the array/dict args
     res.redirect(`/transactions/${transaction._id}`);
 })
 
 app.delete('/transactions/:id', async (req, res) => {
-    const { id } = req.params;
+    const {
+        id
+    } = req.params;
     await Transaction.findByIdAndDelete(id);
     res.redirect('/transactions');
 });
 
+// The app.all method is like global logic
+// https://expressjs.com/en/4x/api.html#app.all 
+// The location of the function is very important as if the routes above don't match
+// then it will be caught here
+app.all('*', (req, res, next) => {
+    next(new ExpressError("Page Not Found", 404))
+    res.send("404!!!");
+
+})
+
 // This could include a bunch of error handling as this is the general route for all errors
 app.use((err, req, res, next) => {
-    res.send("Uh ohhh, well that's embarassing! Something went terribly wrong :( Please return to the home page!");
+    const {
+        statusCode = 500, msg = 'Something went wrong!'
+    } = err;
+    res.status(statusCode).send(msg);
 });
 
 app.listen(3000, () => {
