8f0654d49d80ed47676ecf40d34e14b3ea8110ec
Tyler Chen <chentyler52@gmail.com>
2022-09-06 01:41:07 -0600

    now uploads images to cloudinary

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,12 +1,14 @@
-// Library Imports
-const express = require("express");
-const router = express.Router();
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
-
 // Functions and Classes
 const catchAsync = require("../utils/CatchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+const { storage } = require("../cloudinary");
+
+// Library Imports
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const upload = multer({ storage });
+
 
 // Controllers
 const transactions = require("../controllers/transactions");
